d83022154be722d80c2c9e356b705c41f0763f8b
Ravisankar Ka <ka.ravisankar@gmail.com>
2023-09-10 20:03:01 +0530

    update final

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=b2496162`;
+    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=b2496162`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
     // console.log(data.Search);
